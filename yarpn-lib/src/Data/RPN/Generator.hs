db3f926352fc8df739831c18b9cb368214cd5d19
Dan Plubell <dplubell@cerner.com>
2015-10-07 10:16:21 -0500

    clean up generator code

--- yarpn-lib/src/Data/RPN/Generator.hs
+++ yarpn-lib/src/Data/RPN/Generator.hs
@@ -1,4 +1,6 @@
-module Data.RPN.Generator where
+module Data.RPN.Generator (generate
+                          , emitInstructions
+                          , emitRawInstructions) where
 
 import Data.RPN.Internal.Types
 import qualified Data.Set as DSE
@@ -6,12 +8,15 @@ import qualified Data.Sequence as DSEQ
 import qualified Data.Foldable as DF
 import Control.Monad.State
 
+
 type GenState = (DSEQ.Seq Code, DSE.Set Code)
 
 type Generator a = State GenState a
 
+-- | Generate a tuple of codified instructions and the set of symbols
 generate::Tree -> (DSEQ.Seq Code, DSE.Set Code)
 generate t = fst $ runState (generateCodes t) (DSEQ.empty,DSE.empty) 
+
 
 generateCodes::Tree -> Generator (DSEQ.Seq Code, DSE.Set Code)
 generateCodes t = do
@@ -28,11 +33,46 @@ generateCodes t = do
          _ <- generateCodes l
          (cds',syms') <- generateCodes r
          updateState (cds' DSEQ.|> Add 2, syms')
+       SumNode MinusOp l r  -> do
+         _ <- generateCodes l
+         (cds', syms') <- generateCodes r
+         updateState (cds' DSEQ.|> Sub 2, syms')
+       OperationNode MinOp l r -> do
+         _ <- generateCodes l
+         (cds', syms') <- generateCodes r
+         updateState (cds' DSEQ.|> Min 2, syms')
+       OperationNode MaxOp l r -> do
+         _ <- generateCodes l
+         (cds', syms') <- generateCodes r
+         updateState (cds' DSEQ.|> Max 2, syms')
+       UnaryNode PlusOp l       -> do
+         (cds', syms') <-  generateCodes l
+         updateState (cds' DSEQ.|> Pos 1, syms')
+       UnaryNode MinusOp l -> do
+         (cds', syms') <-  generateCodes l
+         updateState (cds' DSEQ.|> Neg 1, syms') 
+       ProdNode TimesOp l r      -> do
+         _ <- generateCodes l
+         (cds' , syms') <- generateCodes  r
+         updateState (cds' DSEQ.|> Mul 2, syms')
+       ProdNode DivOp l r -> do
+         _ <- generateCodes l
+         (cds',syms') <-  generateCodes r
+         updateState (cds' DSEQ.|> Div 2, syms')
+       ProdNode ModOp l r -> do
+         _ <- generateCodes l
+         (cds',syms') <-  generateCodes r
+         updateState (cds' DSEQ.|> Mod 2, syms')
+       ProdNode PowOp l r -> do
+         _ <- generateCodes l
+         (cds', syms') <- generateCodes r
+         updateState (cds' DSEQ.|> Pow 2, syms')
+       _                  -> error "Error generating instructions"
 
    where updateState state' = do
            put state'
            return state'
-        
+{-        
 generate'::(DSEQ.Seq Code, DSE.Set Code) -> Tree -> (DSEQ.Seq Code, DSE.Set Code)
 generate' (cds,syms) t =
   case t of
@@ -74,7 +114,7 @@ generate' (cds,syms) t =
       let (cds', syms') = generate' (generate' (cds, syms) l ) r
       in (cds' DSEQ.|> Pow 2, syms')
     _                  -> error "Error generating instructions"
-
+-}
 emitInstructions::(DSEQ.Seq Code, DSE.Set Code) -> [String]
 emitInstructions (cds,syms) = map show (DSE.toList syms) ++ map show (DF.toList cds)
 
