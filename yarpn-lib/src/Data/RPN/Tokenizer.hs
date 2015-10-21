f77ef336721b371de24e66463b6069bafeea84ca
Dan Plubell <dplubell@cerner.com>
2015-10-21 14:48:11 -0500

    fix tokenizing of power

--- yarpn-lib/src/Data/RPN/Tokenizer.hs
+++ yarpn-lib/src/Data/RPN/Tokenizer.hs
@@ -1,17 +1,17 @@
 module Data.RPN.Tokenizer (tokenize) where
 
-import Data.RPN.Internal.Types
-import Data.Char
+import           Data.Char
+import           Data.RPN.Internal.Types
 
 -- | Take in a string of characters and tokenize them
-   
+
 -- | Convert a character to an abstract parenthese
 paren :: Char -> Paren
 paren c | c == '(' = LeftParen
         | c == ')' = RightParen
         | otherwise = NotParen
 
--- | Convert a character to an abstract operator      
+-- | Convert a character to an abstract operator
 operator :: Char -> Operator
 operator c | c == '+' = PlusOp
            | c == '-' = MinusOp
@@ -22,30 +22,30 @@ operator c | c == '+' = PlusOp
            | otherwise = NotOp
 
 -- | Convert a character to a token
-tokenizeChar::Char -> Token 
-tokenizeChar c 
+tokenizeChar::Char -> Token
+tokenizeChar c
   | elem c "()"    = TokenParen (paren c)
-  | elem c "-+/*%" = TokenOp (operator c)
+  | elem c "-+/*%^" = TokenOp (operator c)
   | c == '='        = TokenAssign
   | isSpace c      = TokenWhiteSpace
   | otherwise      = TokenInvalid $ "Cannot tokenize: " ++ [c]
-                     
+
 -- | Convert to an operation or identifier
 identifier:: Char -> [Char] -> [Token]
 identifier c cs = let (str,cs') = span isAlphaNum cs in
-                        operationOrIdent (c:str) : tokenize cs'   
-                  where operationOrIdent str' 
+                        operationOrIdent (c:str) : tokenize cs'
+                  where operationOrIdent str'
                           | str' == "min" = TokenOper MinOp
                           | str' == "max" = TokenOper MaxOp
                           | otherwise    = TokenSymbol str'
 
--- | Convert to number represented as a double 
-number :: Char -> [Char] -> [Token]                                                 
-number c cs = 
+-- | Convert to number represented as a double
+number :: Char -> [Char] -> [Token]
+number c cs =
    let (digs, cs') = span isDecimalOrDigit cs in
        cnvtToTokenNum (c:digs) : tokenize cs'
    where isDecimalOrDigit c' = isDigit c' || c' == '.'
-         cnvtToTokenNum s = TokenNumber (read s::Double) 
+         cnvtToTokenNum s = TokenNumber (read s::Double)
 
 -- | Convert a string to a list of tokens
 tokenizer :: String -> [Token]
@@ -57,4 +57,4 @@ tokenizer (c:cs)
 
 -- | Convert a string to a filtered list of tokens
 tokenize :: String -> [Token]
-tokenize = filter (\t -> t /= TokenWhiteSpace) . tokenizer 
+tokenize = filter (\t -> t /= TokenWhiteSpace) . tokenizer
--- yarpnc/src/Main.hs
+++ yarpnc/src/Main.hs
@@ -1,22 +1,64 @@
 module Main where
 
+import           Control.Applicative   ((<$>))
+import           Control.Monad
 import           Data.RPN.Api
+import           System.Console.GetOpt
+import           System.Environment
 import           System.IO
 
-main :: IO ()
-main = loop
+-- | abstract values for the flags
+data Flag = TokenizeOnly -- optional
+          | ParseOnly    -- optional
+          deriving (Show)
+-- | descriptions of the flags
+options:: [OptDescr Flag]
+options =
+  [ Option "t" ["tokenize"] (NoArg TokenizeOnly) "output list of tokens"
+  , Option "p" ["parse"]    (NoArg ParseOnly)    "output syntax tree"
+  ]
 
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case getOpt Permute options args of
+    (_,_,msgs@(_:_))       -> error $ concat msgs ++ usageMsg
+    (flags,fileNames,_)    -> handleFlags flags fileNames
+
+
+handleFlags:: [Flag] -> [FilePath]   -> IO ()
+handleFlags flgs files = do
+  print flgs
+  print files
+
+
+
+usageMsg::String
+usageMsg = usageInfo "yarpnc <inputFile> <outputFile> [OPTIONS]" options
+--  input <- fmap (map  (emitInstructions.generate.parse.tokenize)  ) (takeWhile (not.null).lines <$> getContents)
+--  input <- parse.takeWhile (not.null).lines <$>   getContents
+--  print input
+--     input <- takeWhile (not.null).lines <$> getContents
+
+
+  --     input <- takeWhile (/= "q").lines <$> getContents
+--     print $ map (parse.tokenize) input
+--     print (concatMap tokenize input)
+{-
 loop :: IO ()
 loop = do
+  args <- getArgs
   line <- getLine
   eof <- isEOF
-  if eof || null line
-     then return ()
-     else do
-          c <- processLine line
-          mapM_ putStrLn c
-          loop
+  unless (eof || null line) $
+    do
+     c <- processLine line
+     mapM_ putStrLn c
+     loop
 
-
+-}
+{-
 processLine::String -> IO [String]
 processLine s = return $ (emitInstructions.generate.parse.tokenize) s
+-}
